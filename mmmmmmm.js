062238b36358fdf4a94a5b484a41347211569124
Jakub <kubaloj222@gmail.com>
2024-10-22 09:27:39 +0200

    kommmmit

--- mmmmmmm.js
+++ mmmmmmm.js
@@ -16,5 +16,24 @@ let lista_kontaktow = [
     {
         Name:"Helen Richards",
         Phone:"0800 1111",
+        Email:"libero@convallis.edu",
     }
 ];
+let imie1 = lista_kontaktow[0].Name;
+let imie2 = lista_kontaktow[1].Name;
+let imie3 = lista_kontaktow[2].Name;
+let numerTel1 = lista_kontaktow[0].Phone;
+let numerTel2 = lista_kontaktow[1].Phone;
+let numerTel3 = lista_kontaktow[2].Phone;
+let mail1 = lista_kontaktow[0].Email;
+let mail2 = lista_kontaktow[1].Email;
+let mail3 = lista_kontaktow[2].Email;
+
+let kontakt = {
+    Name:"Maisie Haley",
+    Phone:"0913 521 3030",
+    Email:"risus.Quisque@urna.ca",
+};
+lista_kontaktow.push(kontakt);
+console.log(lista_kontaktow[0]);
+console.log(lista_kontaktow[lista_kontaktow.length-1]);
