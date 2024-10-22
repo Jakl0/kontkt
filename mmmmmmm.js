f4c7b97118a477558979f9a8c77cdeef72595596
Jakub <kubaloj222@gmail.com>
2024-10-22 09:07:57 +0200

    second commit

--- mmmmmmm.js
+++ mmmmmmm.js
@@ -1,1 +1,20 @@
-let lista_kontaktow = [];
+console.log();
+console.log('Numer Zadania');
+console.log('----------------');
+console.log();
+let lista_kontaktow = [
+    {
+        Name:"gabriel steniatyn",
+        Phone:"(0191) 719 6495",
+        Email:"Curabitur.egestas.nunc@nonummyac.co.uk",
+    },
+    {
+        Name:"Raja Villarreal",
+        Phone:"0866 398 2895",
+        Email:"posuere.vulputate@sed.com",
+    },
+    {
+        Name:"Helen Richards",
+        Phone:"0800 1111",
+    }
+];
